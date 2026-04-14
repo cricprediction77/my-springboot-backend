1be6ec2512177e75efa0299df87e5c5865acbd3f
cricprediction77 <cricprediction77@gmail.com@gmail.com>
2026-04-14 18:23:36 +0530

    time changed to 30mins and count up to 30

--- src/main/java/com/crictpredict/predictbe/scheduler/CounterScheduler.java
+++ src/main/java/com/crictpredict/predictbe/scheduler/CounterScheduler.java
@@ -12,7 +12,7 @@ public class CounterScheduler {
     private CounterService counterService;
 
     // Every 1 hour
-    @Scheduled(cron = "*/30 * * * * *")
+    @Scheduled(cron = "0 */30 * * * *")
     public void runEveryHour() {
         counterService.updateCounter();
     }
--- src/main/java/com/crictpredict/predictbe/service/CounterService.java
+++ src/main/java/com/crictpredict/predictbe/service/CounterService.java
@@ -40,7 +40,7 @@ public class CounterService {
             counter.setLastUpdatedDate(LocalDate.now());
         }
 
-        int random = new Random().nextInt(101); // 0–31
+        int random = new Random().nextInt(31); // 0–31
 
         counter.setLastGeneratedCount(random);
         counter.setTodayCount(counter.getTodayCount() + random);
